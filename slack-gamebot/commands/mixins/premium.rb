291e857ce6080fb5f8ac0cce56d4fd4783c0262e
Frederik Kuehn <fkuehn@xmatters.com>
2016-08-03 17:46:58 -0700

    remove premium

--- slack-gamebot/commands/mixins/premium.rb
+++ slack-gamebot/commands/mixins/premium.rb
@@ -7,22 +7,12 @@ module SlackGamebot
         module ClassMethods
           def premium_command(*values, &_block)
             command(*values) do |client, data, match|
-              if Stripe.api_key && !client.owner.reload.premium
-                client.say channel: data.channel, text: client.owner.premium_text
-                logger.info "#{client.owner}, user=#{data.user}, text=#{data.text}, premium feature required"
-              else
-                yield client, data, match
-              end
+              yield client, data, match
             end
           end
 
           def premium(client, data, &_block)
-            if Stripe.api_key && !client.owner.reload.premium
-              client.say channel: data.channel, text: client.owner.premium_text
-              logger.info "#{client.owner}, user=#{data.user}, text=#{data.text}, premium feature required"
-            else
-              yield
-            end
+            yield
           end
         end
       end
